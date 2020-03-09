fc68051d082f1e9d51af70eda76b0e05c626b954
Ming Dai <mingdai31@gmail.com>
2020-03-09 00:48:38 -0700

    refresh loop

--- puppet.js
+++ puppet.js
@@ -4,12 +4,10 @@ const TICKET_URL = 'https://www.nike.com/launch/t/lebron-7-fairfax/'
 // const TICKET_URL = 'https://www.nike.com/launch/t/sacai-ldv-waffle-white/'
 // const TICKET_URL = 'https://www.nike.com/launch/t/womens-waffle-racer-off-white-athlete-in-progress-vivid-sky/
 
-_tryAddToCart = async (page, num, startTime) => {
+_trySelectSize = async (page) => {
   try {
-    console.log(`try ${num}`);
-
-    console.log("sleep before selecting shoe size");
-    await new Promise(r => setTimeout(r, 1000));
+    console.log("let the page load a lil");
+    await new Promise(r => setTimeout(r, 1500));
 
     await page.evaluate(() => {
       const SHOE_SIZE_BTN = {
@@ -28,67 +26,92 @@ _tryAddToCart = async (page, num, startTime) => {
       });
       shoeSizeBtn.click();
     });
-    await page.evaluate(() => {
-      const ADD_CART_BTN = {
-        BOLD_TEXT: 'ncss-brand',
-        TEXT: 'ADD TO CART'
-      }
+    return true;
+  } catch (e) {
+    console.log(e);
+    page.reload();
+  } 
+}
+
+_tryAddToCart = async (page, num, startTime) => {
+  try {
+    console.log(`try ${num}`);
+    if (num > 1000){ return true; }
+
+    const ADD_CART_BTN = {
+      BOLD_TEXT: 'ncss-brand',
+      TEXT: 'ADD TO CART'
+    }
+    const buyButton = await page.waitFor((ADD_CART_BTN) => {
       const possibleAddCartBtns = document.querySelectorAll(
         `button[class~=${ADD_CART_BTN.BOLD_TEXT}]`
       );
-      let addCartBtn = null;
+      
+      let addToCartBtn = null;
       possibleAddCartBtns.forEach((btn) => {
         if (btn.innerText.includes(ADD_CART_BTN.TEXT)){
-          addCartBtn = btn;
+          addToCartBtn = btn;
         }
       });
-      addCartBtn.click({clickCount: 2});
-    });
+      addToCartBtn.click({clickCount: 3, delay: 500});
+      return true
+    }, {}, ADD_CART_BTN);
+
     const addedCarttime = new Date().getTime();
     console.log(`Added to cart in: ${(addedCarttime - startTime)/1000} seconds`);
 
+    // if (buyButton); buyButton.click();
     console.log("sleep before selecting checkout pop up modal");
-    await new Promise(r => setTimeout(r, 3000));
+    await new Promise(r => setTimeout(r, 1500));
+    // if (buyButton); buyButton.click();
 
     await page.evaluate(() => {
       const CHECKOUT_BTN = {
         MODAL_CLASS: 'cart-link',
         TEXT: 'CHECKOUT'
       }
-      const possibleAddCartBtns = document.querySelectorAll(
+      const checkoutModalBtns = document.querySelectorAll(
         `button[class~=${CHECKOUT_BTN.MODAL_CLASS}]`
       );
-      let addCartBtn = null;
-      possibleAddCartBtns.forEach((btn) => {
+      let checkoutBtn = null;
+      checkoutModalBtns.forEach((btn) => {
         if (btn.innerText.includes(CHECKOUT_BTN.TEXT)){
-          addCartBtn = btn;
+          checkoutBtn = btn;
         }
       });
-      addCartBtn.click();
+      checkoutBtn.click();
     });
 
     return true;
   } catch (e) {
+    //swallow errors
     console.log(e);
-    page.reload();
   }
 }
 
 const run = async () => {
-  const browser = await puppeteer.launch({headless: false, devtools: true});
+  const browser = await puppeteer.launch({
+    headless: false, 
+    devtools: true
+    // executablePath: '/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome'
+  });
   const page = await browser.newPage();
   await page.goto(TICKET_URL);
   // await page.waitForNavigation();
 
+  let selectedSize = null;
+  while (!selectedSize) {
+    selectedSize = await _trySelectSize(page);
+  }
+
   let num = 0;
-  let addedToCart = null;
   const startTime = new Date().getTime();
+  let addedToCart = null;
   while (!addedToCart) {
     num++;
-    addedToCart = await _tryAddToCart(page, num, startTime);
+    addedToCart = await _tryAddToCart(page, num, startTime)
   }
-  console.log("ADDED TO CART!");
-  
+
   console.log("sleep before selecting guest checkout");
   await new Promise(r => setTimeout(r, 2000));
   try{
@@ -101,4 +124,3 @@ const run = async () => {
 }
 
 run();
-
